b88d70f1637dbb5272780103bd9ec14e1cf9b686
Egor Kartashov <68367096+WildEgor@users.noreply.github.com>
2021-05-09 15:15:58 +0600

    fix

--- src/common/FormControls/FormControls.jsx
+++ src/common/FormControls/FormControls.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-import s from "./FormsControls.module.css";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  input: {
+    width: '100%',
+  },
+  textarea: {
+    width: '100%',
+  },
+  field: {
+    position: 'relative'
+  },
+  error: {
+    border: '2px solid red'
+  },
+  errorText: {
+    position: 'absolute',
+    top: '0',
+    right: '0',
+    height: '16px',
+    color: 'rgb(255, 0, 0)',
+    opacity: '0',
+    'font-size': '12px',
+    'margin-bottom': '5px',
+  },
+  errorOn: {
+    opacity: '1'
+  }
+}))
 
 export const Element = (Element) => (props) => {
+  const s = useStyles()
   const { input, meta } = props;
   const hasError = meta.touched && meta.error;
   return (
